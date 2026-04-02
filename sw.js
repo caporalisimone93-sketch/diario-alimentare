a3c9241c81e640079e115c6b4e80ddd0b5981eb2
caporalisimone93-sketch <caporali.simone93@gmail.com>
2026-04-02 21:16:06 +0200

    Update CACHE_NAME to 'diet-store-v6'

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'diet-store-v5'; // incrementa questo numero ad ogni modifica
+const CACHE_NAME = 'diet-store-v6'; // incrementa questo numero ad ogni modifica
 
 self.addEventListener('install', (e) => {
     e.waitUntil(
