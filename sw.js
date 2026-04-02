8bc974959699a71f2498d995b4870eed4626f2ab
caporalisimone93-sketch <caporali.simone93@gmail.com>
2026-04-02 20:57:42 +0200

    Update service worker to version 3

--- sw.js
+++ sw.js
@@ -1,24 +1,23 @@
-const CACHE_NAME = 'diet-store-v2'; // Incrementa questa versione ad ogni deploy
+const CACHE_NAME = 'diet-store-v3'; // incrementa questo numero ad ogni modifica
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(['index.html', 'app.js']))
-  );
-  self.skipWaiting(); // Attiva subito il nuovo SW senza aspettare
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(['index.html', 'app.js']))
+    );
+    self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
-  // Elimina tutte le cache vecchie
-  e.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
-    )
-  );
-  self.clients.claim(); // Prende controllo di tutte le tab aperte subito
+    e.waitUntil(
+        caches.keys().then((keys) =>
+            Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+        )
+    );
+    self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
-  );
+    e.respondWith(
+        caches.match(e.request).then((response) => response || fetch(e.request))
+    );
 });
